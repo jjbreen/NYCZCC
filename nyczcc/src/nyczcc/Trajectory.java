484555588cce283ca65f4f4325953419aa0e5447
John Breen <jjbreen@wpi.edu>
2016-03-19 10:50:32 +0000

    Added getters to Trajectory class

--- nyczcc/src/nyczcc/Trajectory.java
+++ nyczcc/src/nyczcc/Trajectory.java
@@ -1,5 +1,43 @@
 package nyczcc;
 
+import java.time.LocalDateTime;
+
 public class Trajectory {
+	private LocalDateTime pickupt;
+	private LocalDateTime dropofft;
+	private double plat;
+	private double plong;
+	private double dlat;
+	private double dlong;
+	
+	public Trajectory(String pickupt, String dropofft, double plat, double plong, double dlat, double dlong)
+	{
+		
+	}
+	
+	public LocalDateTime getPickUpTime(){
+		return pickupt;
+	}
+	
+	public LocalDateTime getDropOffTime(){
+		return dropofft;
+	}
+	
+	public double getPickUpLongitude(){
+		return plong;
+	}
+	
+	public double getPickUpLatitude(){
+		return plat;
+	}
+	
+	public double getDropOffLatitude(){
+		return dlat;
+	}
+	
+	public double getDropOffLongitude(){
+		return dlong;
+	}
+	
 
 }
